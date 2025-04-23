a5077fa1d83ef7ef47fbaadf102225ebf147cc56
raghav02468 <156989294+raghav02468@users.noreply.github.com>
2025-04-23 15:45:41 +0530

    Add files via upload

--- LeafNodesTree.java
+++ LeafNodesTree.java
@@ -1,5 +1,35 @@
 package file_submision;
 
-public class LeafNodesTree {
+class Node {
+    int data;
+    Node left, right;
 
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
 }
+
+public class LeafNodesTree {
+    Node root;
+
+    int countLeafNodes(Node node) {
+        if (node == null)
+            return 0;
+        if (node.left == null && node.right == null)
+            return 1;
+        return countLeafNodes(node.left) + countLeafNodes(node.right);
+    }
+
+    public static void main(String[] args) {
+        LeafNodesTree tree = new LeafNodesTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.println("Number of leaf nodes is : " + tree.countLeafNodes(tree.root));
+    }
+}
+
