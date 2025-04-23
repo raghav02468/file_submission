7db6bd741a8dbac13253b3b9d4536ad668a829ba
raghav02468 <156989294+raghav02468@users.noreply.github.com>
2025-04-23 15:44:52 +0530

    Add files via upload

--- CountNodesTree.java
+++ CountNodesTree.java
@@ -1,5 +1,32 @@
 package file_submision;
 
-public class CountNodesTree {
+class Node {
+    int data;
+    Node left, right;
 
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class CountNodesTree {
+    Node root;
+
+    int countNodes(Node node) {
+        if (node == null)
+            return 0;
+        return 1 + countNodes(node.left) + countNodes(node.right);
+    }
+
+    public static void main(String[] args) {
+        CountNodesTree tree = new CountNodesTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.println("Total number of nodes is : " + tree.countNodes(tree.root));
+    }
 }
